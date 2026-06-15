e934a21643897c36b188e719493a38e25c01fe0c
HotmangasiM <hotmangasi25@gmail.com>
2026-06-15 22:53:32 +0700

    Day 1

--- WEEK 1/Day 1/Code/Main.java
+++ WEEK 1/Day 1/Code/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        // System.out.println(10);
+        // System.out.println(10 + 10);
+        // System.out.println(10 % 10);
+        // System.out.println(10 * 10);
+        // System.out.println(10 - 10);
+        // final String name = "Hotmangasi";
+        // // name = "Manurung";
+        // System.out.println(name);
+
+        // String name = "Hotmangasi";
+        // System.out.println(name + " Manurung");
+
+        // String firstName = "Hotmangasi";
+        // String lastName = "Manurung";
+
+        // int x = 1;
+        // int y = 10;
+
+        // System.out.println("The sum is " + x + y);
+        // System.out.println("The sum is "+ (x + y));
+
+        int x, y, z;
+        x = y = z = 20;
+        System.out.println(x + y + z);
     }
 }
---
+++ WEEK 1/Day 1/Code/Test.java
@@ -0,0 +1,23 @@
+public class Test {
+    public static void main (String[] args){
+        // String studentName = "Hotmangasi Manurung";
+        // int studentID = 15;
+        // int studentAge = 24;
+        // float studentFee = 75.25f;
+        // char studentGrade = 'A';
+
+        // System.out.println("Student name "+ studentName);
+        // System.out.println("Student id "+ studentID);
+        // System.out.println("Student age "+ studentAge);
+        // System.out.println("Student fee "+ studentFee);
+        // System.out.println("Student grade "+ studentGrade);
+
+        int length = 4;
+        int width = 6;
+        int area;
+
+        area = length * width;
+
+        System.out.println("This is the area " + area);
+    }
+}
