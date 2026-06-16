3b65d74d545c92ec26ae382946e18926e678f733
HotmangasiM <hotmangasi25@gmail.com>
2026-06-16 10:33:35 +0700

    Day 2

---
+++ WEEK 1/Day 2/Code/Main.java
@@ -0,0 +1,26 @@
+// package WEEK 1.Day 2.Code;
+
+public class Main {
+    public static void main(String[] args){
+        byte myNum = 100;
+        short myNum2 = 5000;
+        int myNum3 = 2147483647;
+        long myNum4 = 15000000000L;
+        float dec1 = 3.45f;
+        double doub1 = 3.564d;
+        boolean isJavaisFun = true;
+        boolean isFishTasty = false;
+        char myGrade = 'A';
+        String myGreetings = "Hello Hotmangasi";
+        System.out.println("This is the byte number : "+ myNum);
+        System.out.println("This is the short number: "+ myNum2);
+        System.out.println("This the integer number : "+ myNum3);
+        System.out.println("This the long number : "+ myNum4);
+        System.out.println("This the decimal number : "+ dec1);
+        System.out.println("This the double number : "+ doub1);
+        System.out.println(isJavaisFun);
+        System.out.println(isFishTasty);
+        System.out.println(myGrade);
+        System.out.println(myGreetings);
+    }
+}
