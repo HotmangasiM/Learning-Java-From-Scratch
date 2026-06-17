bf87a2e17a9ed2577e8a1a93b22a719145c77fee
HotmangasiM <hotmangasi25@gmail.com>
2026-06-17 09:35:36 +0700

    update learning for day 2

--- WEEK 1/Day 2/Code/Excercise.java
+++ WEEK 1/Day 2/Code/Excercise.java
@@ -2,15 +2,33 @@
 
 public class Excercise {
     public static void main (String[] args){
-        int items = 50;
-        float costPerItem = 9.99f;
-        float totalCost = items * costPerItem;
-        char currency = '$';
+        // int items = 50;
+        // float costPerItem = 9.99f;
+        // float totalCost = items * costPerItem;
+        // char currency = '$';
 
-        // Print variables
-        System.out.println("Number of items: "+ items);
-        System.out.println("Cost per items: "+ costPerItem + currency);
-        System.out.println("Total cost= "+ totalCost + currency);
+        // var a = 'a';
+
+        // // Print variables
+        // System.out.println("Number of items: "+ items);
+        // System.out.println("Cost per items: "+ costPerItem + currency);
+        // System.out.println("Total cost= "+ totalCost + currency);
+
+        // System.out.println(a);
+
+
+        // Widening Casting
+        int number = 10;
+        double number1 = number;
+        float number2 = number;
+
+        System.out.println(number2);
+
+        // Narrowing Casting
+        double result = 15.46d;
+        int intResult = (int) result;
+
+        System.out.println(intResult);
 
     }
 }
---
+++ WEEK 1/Day 2/Code/Operator.java
@@ -0,0 +1,23 @@
+// package WEEK 1.Day 2.Code;
+
+public class Operator {
+    public static void main(String[] args){
+        int x = 10;
+        int y = 3;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+        System.out.println(x % y);
+
+        int z = 5;
+        ++z;
+        System.out.println(z);
+
+        --z;
+        System.out.println(z);
+
+        // System.out.println(x);
+    }
+}
