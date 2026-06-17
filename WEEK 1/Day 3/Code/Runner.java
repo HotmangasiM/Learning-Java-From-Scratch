d2391f6d099f4e07a6550c03bf9f6fe4a89f25c6
HotmangasiM <hotmangasi25@gmail.com>
2026-06-17 16:03:51 +0700

    Update Day 3: 17-06-2026

---
+++ WEEK 1/Day 3/Code/Runner.java
@@ -0,0 +1,85 @@
+// package WEEK 1.Day 3.Code;
+
+public class Runner {
+    public static void main(String[] args){
+        // boolean isLoggedIn = true;
+        // boolean isAdmin = false;
+
+        // System.out.println("Regular user: "+ (isLoggedIn && !isAdmin));
+        // System.out.println("Has access: "+ (isLoggedIn || isAdmin));
+        // System.out.println("Not logged in: "+ (!isLoggedIn));
+
+        // int result1 = 10 - 2 + 5;
+        // int result2 = 10 - (1 + 5);
+
+        // System.out.println(result1);
+        // System.out.println(result2);
+
+        // String fullName = "Hotmangasi Manur ung  ";
+        // String lastName = "   Manurung Hotmangasi   ";
+
+        // System.out.println(fullName.indexOf("Hotmangasi"));
+
+        // System.out.println(fullName.charAt(0));
+        // System.out.println(fullName.trim());
+        // System.out.println(fullName.equals(lastName));
+
+        // System.out.println("Length of the string is: "+ fullName.length());
+        // System.out.println("Uppercase string: "+ fullName.toUpperCase());
+        // System.out.println("Lowercase string: "+ fullName.toLowerCase());
+        // System.out.println(fullName.indexOf("Hotmangasi"));
+
+        String firstName = "Hotmangasi";
+        String lastName = "Manurung";
+        String anotherName = "MrKhong";
+
+        // String fullName = firstName + lastName;
+
+        // System.out.println(firstName + (" ")+ lastName);
+
+        // String name = "Hotmangasi";
+        // int age = 24;
+
+        // System.out.println("My name is "+ name + " and I am "+ age + " years old");
+
+        // System.out.println(firstName.concat(lastName).concat(anotherName));
+
+
+        // int number = 10;
+        // String name = "Hotmangasi";
+
+        // System.out.println(name + number);
+
+        // String text = "contoh kalimat langsung \"Halo perkenalkan nama saya hotmangasi manurung yang sedang belajar bahasa pemrograman Java\" yang saat ini sedang dikantor";
+        // String singleQuote = "It\'s alright";
+        // String backslash = "The character \\ is backslash";
+
+        // System.out.println("textnya: "+ text);
+        // System.out.println("Single Quotes: "+ singleQuote);
+        // System.out.println("Backslash: "+ backslash);
+
+        // System.out.println(Math.max(5, 10));
+        // System.out.println(Math.min(5, 10));
+        // System.out.println(Math.sqrt(64));
+        // System.out.println(Math.abs(-4.7));
+        // System.out.println(Math.pow(2, 3));
+
+        // // Rounding
+        // System.out.println(Math.round(4.6));
+        // System.out.println(Math.ceil(4.1));
+        // System.out.println(Math.floor(4.9));
+
+        // System.out.println((int)(Math.random()* 101));
+
+        // boolean isRaining = true;
+
+        // if (isRaining){
+        //     System.out.println("It\'s rain, take your umbrella!!!");
+        // }
+
+        int time = 20;
+        String result = (time < 18) ? "Good day" : "Good evening";
+        System.out.println(result);
+
+    }
+}
