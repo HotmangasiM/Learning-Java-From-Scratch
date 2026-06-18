fc7e6dfa23d1ec1316358aad0a66136e2b388819
HotmangasiM <hotmangasi25@gmail.com>
2026-06-19 06:08:15 +0700

    push for day 4

---
+++ WEEK 1/Day 4/Code/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    // static void myMethod(){
+    //     System.out.println("Hello Hotmangasi");
+    // }
+
+    static void myMethod(String fname){
+        System.out.println(fname + " Refnes");
+    }
+    
+    public static void main(String[] args){
+        // myMethod();
+        myMethod("Hotmangasi");
+        myMethod("Manurung");
+        myMethod("Simorangkir");
+    }
+}
